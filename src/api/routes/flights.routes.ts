656d56af64261771327b1b144da01f9028ed7465
duyen26912 <nguyenduyen.260903@gmail.com>
2025-05-31 15:12:44 +0700

    Refactor flight routes to improve organization and clarity. Grouped routes by user roles and added a new endpoint for searching flights by price.

--- src/api/routes/flights.routes.ts
+++ src/api/routes/flights.routes.ts
@@ -3,30 +3,30 @@ const router = express.Router();
 const flightController = require('../../controllers/flights.controller');
 const authMiddleware = require('../middlewares/auth.middleware')
 
-
+// user
 // Lấy tất cả chuyến bay
 router.get('/searchAll', flightController.getAllFlights);
 
 // Lấy 5 chuyến bay phổ biến nhất
 router.get('/popular', flightController.getPopularFlights);
 
-// Lấy chuyến bay theo điểm đi, điểm đến: searchDes?from=1&&to=2
+// Lấy chuyến bay theo điểm đi, điểm đến: searchDes?from=1&to=2
 router.get('/searchDes', flightController.searchFlightsByDes);
 
-// Cập nhật chuyến bay theo ID
-router.put('/update/:id', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), flightController.updateFlight);
-
-// Xoá chuyến bay theo ID
-router.delete('/del/:id',  flightController.deleteFlight);
-
-// Tạo chuyến bay (Khi user là admin)
-router.post('/new', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), flightController.createFlight
-);
+// Lọc chuyến bay theo giá: ?minPrice=1000000&maxPrice=5000000
+router.get('/searchPrice', flightController.searchFlightsByPrice);
 
 // Lấy chuyến bay theo ID
 router.get('/:id', flightController.getFlightById);
 
-// Lọc chuyến bay theo giá: ?minPrice=1000000&maxPrice=5000000
-router.get('/searchPrice', flightController.searchFlightsByPrice);
+// admin
+// Cập nhật chuyến bay theo ID(admin)
+router.put('/update/:id', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), flightController.updateFlight);
+
+// Xoá chuyến bay theo ID(admin)
+router.delete('/del/:id', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), flightController.deleteFlight);
+
+// Tạo chuyến bay (admin)
+router.post('/new', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), flightController.createFlight);
 
 export default router;
