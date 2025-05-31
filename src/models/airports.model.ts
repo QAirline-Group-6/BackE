2ebe9ee82bc930d33dcf6dabe8d3c78d51fa6b28
duyen26912 <nguyenduyen.260903@gmail.com>
2025-05-31 15:14:24 +0700

    Add airport model definition with Sequelize initialization, including attributes for airport_id, code, name, city, and country.

---
+++ src/models/airports.model.ts
@@ -0,0 +1,54 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+
+interface AirportAttributes {
+    airport_id: number;
+    code: string;
+    name: string;
+    city: string;
+    country: string;
+}
+
+interface AirportCreationAttributes extends Optional<AirportAttributes, "airport_id"> { }
+
+
+export class Airport extends Model<AirportAttributes, AirportCreationAttributes> implements AirportAttributes {
+    public airport_id!: number;
+    public code!: string;
+    public name!: string;
+    public city!: string;
+    public country!: string;
+}
+export default (sequelize: Sequelize) => {
+    Airport.init(
+        {
+            airport_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            code: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            city: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            country: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
+        },
+        {
+            sequelize,
+            tableName: 'airports',
+            timestamps: false,
+        }
+    );
+    return Airport;
+}
