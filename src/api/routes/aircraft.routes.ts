946fb7935965ad75585466279afecae901af636f
Tomoriu Akarin <lequocanh10000@github.com>
2025-06-08 00:25:30 +0700

    feat: add CRUD routes for aircrafts including get, update, and delete

--- src/api/routes/aircraft.routes.ts
+++ src/api/routes/aircraft.routes.ts
@@ -7,4 +7,13 @@ const authMiddleware = require('../middlewares/auth.middleware')
 // Tạo máy bay
 router.post('/', authMiddleware.authenticateToken, authMiddleware.authorizeRoles('admin'), aircraftController.createAircraft);
 
+// Lấy danh sách máy bay 
+router.get('/', aircraftController.getAllAircrafts);
+
+// Cập nhật thông tin tàu bay
+router.put('/update/:id', aircraftController.updateAircraft);
+
+// Xóa tàu bay
+router.delete('/del/:id', aircraftController.deleteAircraft);
+
 export default router;
